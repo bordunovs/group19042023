d11900d03fe74699ffd129c04f837744d513d194
Serhii <bordunovs@gmail.com>
2023-04-28 12:53:27 +0300

    дз!

---
+++ strings.py
@@ -0,0 +1,7 @@
+smile_footprint = '\N{Footprints}'
+
+user_name = input('Please, enter your name >>> ')
+user_age =input ('Please, enter your age >>> ')
+result = 'My name is ' + user_name +", I am " + user_age + ' years old' + smile_footprint
+
+print (result)
