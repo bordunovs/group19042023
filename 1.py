88f14cc31d2ff29fb90fdd875b703312c83f0084
Serhii <bordunovs@gmail.com>
2023-04-28 20:05:32 +0300

    дз2

---
+++ 1.py
@@ -0,0 +1,5 @@
+user_name = input('Введите имя : ').title()
+
+user_surname = input('Введите фамилию : ').upper()
+
+print("Здарова," + user_name + user_surname + " а ты знал,что в твоем имени 6 букв?")
