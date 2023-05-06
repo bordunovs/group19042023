4b71ffea37ba90bedc11180e1735a41368748c5d
Serhii <bordunovs@gmail.com>
2023-05-06 17:02:12 +0300

    HW 8

--- brenching.py
+++ brenching.py
@@ -7,12 +7,27 @@ repeat_password = input('repeat password entry >>>')
 true = True
 false = False
 
-print(bool(password == repeat_password))
 
-if password == repeat_password:
-    print('user is logged in')
-elif password != repeat_password:
+if login.isalpha() and password == repeat_password:
+    print('user is extended at the whim of the instructor , repeat login and password')
+
+else:
+
     print('incorrect')
 
-print('user is extended at the whim of the instructor , repeat login and password')
+log1 = input('enter your login (only letters) >>> ')
 
+pas1 = input('enter password(only letters and numbers) >>> ')
+
+pas2 = input('repeat password entry >>>')
+
+true = True
+
+false = False
+
+if password == pas1:
+    print('user is logged in')
+if log1 == login:
+    print('user is logged in')
+if log1 != login:
+    print('password and login not correct')
