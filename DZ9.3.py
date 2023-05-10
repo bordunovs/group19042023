2d4a512a12a60e26f1d08f3f8c1f60c032827e25
Serhii <bordunovs@gmail.com>
2023-05-10 17:32:56 +0300

    HW 9

--- DZ9.3.py
+++ DZ9.3.py
@@ -1,1 +1,7 @@
-gt
+int_text = input('введите любой текст,с маленьких и заглавных букв >>> ')
+int_text_result = []
+for element in int_text:
+ if element.isupper():
+
+    int_text_result.append(element)
+print(int_text_result)
