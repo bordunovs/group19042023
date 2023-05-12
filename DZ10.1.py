988d1e94773c44a9bddedf5d0482179dea842fd5
Serhii <bordunovs@gmail.com>
2023-05-12 14:55:46 +0300

    HW 10-1 citi

---
+++ DZ10.1.py
@@ -0,0 +1,10 @@
+past_cities = set(input("Введите через пробел города, в которых вы были за прошедшие 10 лет: ").split())
+future_cities = set(input("Введите через пробел города, куда вы хотите поехать в следующие 10 лет: ").split())
+
+common_cities = past_cities.intersection(future_cities)
+
+if common_cities:
+    print("По-видимому, вам очень понравилось в городах: " + ', '.join(common_cities))
+else:
+    print("Вы открыты к чему-то новому.")
+
