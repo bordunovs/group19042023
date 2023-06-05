5042bf9d5343d2d6a7443782ea943959149fb2f0
Serhii Bordunov <bordunovs@gmail.com>
2023-06-05 08:41:40 +0200

    hw20

--- files.py
+++
@@ -1,3 +0,0 @@
-with open('new.txt', mode='r+', encoding='utf-8') as my_file:
-    content = my_file.read()
-    print(content)
---
+++ hw20.py
@@ -0,0 +1,18 @@
+import cv2
+import requests
+
+url = 'https://upload.wikimedia.org/wikipedia/commons/1/19/Europ%C3%A4ischer_Ziesel_in_Hockstellung.jpg'
+
+response = requests.get(url)
+#response.raise_for_status()
+
+with open("image.jpg", "wb") as picture_file:
+    picture_file.write(response.content)
+
+print(response.content)
+
+
+
+
+
+
